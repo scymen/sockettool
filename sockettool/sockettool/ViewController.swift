b22f3b8bf9ab91e0d22dc1b6bbec704ef1267836
Abu <abu@Abu-MBA.local>
2016-07-04 17:34:55 +0800

    init

--- sockettool/sockettool/ViewController.swift
+++ sockettool/sockettool/ViewController.swift
@@ -7,21 +7,48 @@
 //
 
 import Cocoa
+import Foundation
+
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBOutlet weak var outlet_radio_servermodel: NSButton!
+    
+    @IBOutlet weak var outlet_radio_clientmodel: NSButton!
+    
+    
+    @IBAction func action_radSelected(_ sender: AnyObject) {
+        
+        let btn = sender as! NSButton
+        
+        if btn == outlet_radio_clientmodel {
+            NSLog("client")
+        }
+        
+        if btn == outlet_radio_servermodel {
+            NSLog("server 4")
+           
+        }
+    }
+    
+    
+    @IBAction func action_btnOK(_ sender: AnyObject) {
+        
+        NSLog( "ok click")
+        
+    }
+    
 }
 
