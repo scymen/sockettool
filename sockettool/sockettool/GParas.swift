2de634895c14f68345671c358f5ab25eb09948c1
Abu <abu@Abu-MBA.local>
2016-07-07 16:37:59 +0800

    b

--- sockettool/sockettool/GParas.swift
+++ sockettool/sockettool/GParas.swift
@@ -17,20 +17,20 @@ public class GParas {
     static var IP:String? = ""
     static var port:Int32 = 8080
     
-    var addr :InternetAddress? = nil
+    static var addr :InternetAddress? = nil
     
     deinit {
         
     }
     
     
-    func checkParas( ) -> Bool {
+    static  func checkParas( ) -> Bool {
         if GParas.IP == nil {
             return false
         }
         
-        self.addr = try InternetAddress(hostname: GParas.IP!, port: (UInt16)(GParas.port),
-                                        addressFamily: AddressFamily.inet)
+        GParas.addr = try InternetAddress(hostname: GParas.IP!, port: (UInt16)(GParas.port),
+                                          addressFamily: AddressFamily.inet)
         
         return true
     }
@@ -54,8 +54,39 @@ public class GParas {
         } catch {
             print("Error \(error)")
         }
+
+    }
+    
+    static let regular_ip : String = "((?:(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.){3}(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d))"
+    
+    static func isMatch(str :String, pattern :String) -> Bool{
         
+        do {
+            // - 1、创建规则
+            // let pattern = "[1-9][0-9]{1,5}"
+            // - 2、创建正则表达式对象
+            let regex = try RegularExpression(pattern: pattern, options: RegularExpression.Options.caseInsensitive)
+            // - 3、开始匹配
+            let res = regex.matches(in: str, options: RegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, str.characters.count))
+            
+            if res.count > 0 {
+                return true
+            }else {
+                return false
+            }
+            
+            // 输出结果
+            //            for checkingRes in res {
+            //                print((str as NSString).substring(with: checkingRes.range))
+            //            }
+        }
+        catch {
+            print(error)
+            return false
+            
+        }
         
     }
+    
     
 }
--- sockettool/sockettool/ViewController.swift
+++ sockettool/sockettool/ViewController.swift
@@ -12,19 +12,45 @@ import Socks
 
 
 
+
 class ViewController: NSViewController {
+    
+    let txt_cnn : String = "Connect"
+    let txt_cnn_ing : String = "Connecting..."
+    let txt_listen : String = "Listen"
+    let txt_listening : String = "Listening.."
+    let txt_stop : String = "Stop"
+    
+    let arr :[[String]] = [["1.1.1.1","80"],["2.2.2.2","81"],["3.3.3.3","82"]]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
+        
+        if outlet_radio_clientmodel.state == 1 {
+            outlet_btn_ok.title = txt_cnn
+        }
+        
+        outlet_tableview.delegate = self
+        outlet_tableview.dataSource = self
+        
+      //  outlet_tableview.setDelegate(self)
+      //  outlet_tableview.setDataSource(self)
+        
     }
     
     override var representedObject: AnyObject? {
         didSet {
             // Update the view, if already loaded.
+            
         }
     }
+    @IBOutlet weak var outlet_tableview: NSTableView!
+    
+    @IBOutlet weak var outlet_txt_port: NSTextField!
+    
+    @IBOutlet weak var outlet_txt_ip: NSTextField!
     
     @IBOutlet weak var outlet_radio_servermodel: NSButton!
     
@@ -32,26 +58,55 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var outlet_btn_ok: NSButton!
     
+    @IBOutlet weak var outlet_btn_save_as: NSButton!
+    
+    @IBOutlet weak var outlet_btn_clear: NSButton!
+    
+    @IBOutlet weak var outlet_textview: NSScrollView!
+    
+    @IBAction func action_btn_clear(_ sender: AnyObject) {
+        
+        //  outlet_textview.text = ""
+        //  outlet_textview.title = ""
+        //  outlet_textview.stringValue = ""
+        //  outlet_textview.contentView.storage
+        
+        let fuckapple :NSClipView =   outlet_textview.contentView
+        var shit : NSTextView? = nil
+        
+        for n  in fuckapple.subviews {
+            if n.className == "NSTextView" {
+                shit = n as? NSTextView
+                break
+            }
+        }
+        
+        if shit != nil {
+            shit?.string = "wft apple"
+        }
+        
+        
+    }
+    
     @IBAction func action_radSelected(_ sender: AnyObject) {
         
-//        let btn = sender as! NSButton
-//        
-//        if btn == outlet_radio_clientmodel {
-//            NSLog("client")
-//        }
-//        
-//        if btn == outlet_radio_servermodel {
-//            NSLog("server 4")
-//            
-//        }
+        let btn = sender as! NSButton
+        
+        if btn == outlet_radio_clientmodel {
+            outlet_btn_ok.title = txt_cnn
+        }else {
+            outlet_btn_ok.title = txt_listen
+        }
+        // outlet_btn_ok.autoresizesSubviews = true
+        //  if btn == outlet_radio_servermodel {
+        //     NSLog("server 4")
+        //
+        //  }
     }
     
     
+    
     @IBAction func action_btnOK(_ sender: AnyObject) {
-        
-     
-        
-        
         
         if outlet_radio_clientmodel.state==1{
             GParas.isClientMode = true
@@ -59,12 +114,127 @@ class ViewController: NSViewController {
             GParas.isClientMode = false
         }
         
+        if  !GParas.isMatch(str: outlet_txt_port.stringValue,pattern: "^\\d{1,5}$")
+            || !GParas.isMatch(str: outlet_txt_ip.stringValue, pattern: GParas.regular_ip){
+            
+            let a = NSAlert()
+            a.messageText = "Error"
+            a.informativeText = "IP or Port invalidated"
+            a.addButton(withTitle: "OK")
+            // a.addButton(withTitle: "Cancel")
+            a.alertStyle = NSAlertStyle.warning
+            
+            a.beginSheetModal(for: self.view.window!, completionHandler: { (modalResponse) -> Void in
+                if modalResponse == NSAlertFirstButtonReturn {
+                    NSLog(a.informativeText)
+                }
+            })
+            
+            
+        } else {
+            
+            GParas.IP =  outlet_txt_ip.stringValue
+            GParas.port = (Int32)( outlet_txt_port.stringValue)!
+            GParas.checkParas()
+            
+            
+        }
         
-       
-        
-        NSLog( "ok click")
+        // NSLog( "ok click")
         
     }
     
 }
 
+
+extension ViewController : NSTableViewDataSource {
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> AnyObject?{
+        
+//        // var image:NSImage?
+//        var text:String = ""
+//        var cellIdentifier: String = ""
+//        
+//        // 1
+//        //        guard let item = arr[row] else {
+//        //            return nil
+//        //        }
+//        
+//        let item = arr[row]
+//        
+//        // 2
+//        if tableColumn == tableView.tableColumns[0] {
+//            // image = item.icon
+//            text = item[0]
+//            cellIdentifier = "NameCellID"
+//        } else if tableColumn == tableView.tableColumns[1] {
+//            text = item[1]
+//            cellIdentifier = "DateCellID"
+//        } else if tableColumn == tableView.tableColumns[2] {
+//            text = "sizeeee"
+//            cellIdentifier = "SizeCellID"
+//        }
+//        
+//        // 3
+//        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+//            cell.textField?.stringValue = text
+//            // cell.imageView?.image = image ?? nil
+//            return cell
+//        }
+        return nil
+    }
+    
+    
+      func tableView(_ tableView: NSTableView, setObjectValue object: AnyObject?, for tableColumn: NSTableColumn?, row: Int){
+        
+        
+    }
+    
+    //numberOfRowsInTableView
+    func numberOfRows(in tableView: NSTableView) -> Int{
+  //  func numberOfRows(tableView: NSTableView) -> Int {
+        // return directoryItems?.count ?? 0
+        
+        return arr.count
+    }
+    
+}
+
+extension ViewController : NSTableViewDelegate {
+    
+    // called by the table view for every row and column to get the appropriate cell.
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        // var image:NSImage?
+        var text:String = ""
+        var cellIdentifier: String = ""
+        
+        // 1
+        //        guard let item = arr[row] else {
+        //            return nil
+        //        }
+        
+        let item = arr[row]
+        
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            // image = item.icon
+            text = item[0]
+            cellIdentifier = "ipcellID"
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = item[1]
+            cellIdentifier = "portcellID"
+        } else if tableColumn == tableView.tableColumns[2] {
+            text = "sizeeee"
+            cellIdentifier = "statuscellID"
+        }
+        
+        // 3
+        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            // cell.imageView?.image = image ?? nil
+            return cell
+        }
+        return nil
+    }
+}
